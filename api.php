cc62e3acae9c3d7d81c16712c58d4d8fd0724818
Victor Ratts <victor.ratts13@gmail.com>
2020-05-08 18:20:39 -0300

    Update api.php

--- api.php
+++ api.php
@@ -5,7 +5,6 @@ require('./vendor/autoload.php');
 
 use GuzzleHttp\Client;
 
-//Cabeçalho de Requisição
 $api = new Client([
     'headers' => [
         'User-Agent' => 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Mobile Safari/537.36',
@@ -28,12 +27,10 @@ $api = new Client([
 
 $post = $_POST;
 
-//função Principal
-function postFunction356($user356, $pass356, $method)
+function postFunction356($user356, $pass356, $method, $betValue, $hc, $ls)
 {
     global $api, $body;
-    
-    //efetua Login
+
     if ($user356 == true) {
         $api->post(LOGIN, [
             'form_params' => [
@@ -42,8 +39,7 @@ function postFunction356($user356, $pass356, $method)
                 'txtType' => 47
             ]
         ]);
-        
-        //exibe dados de login
+
         if ($method == 'login') {
             $response = $api->get(URI . PROFILE);
             $content = $response->getBody()->getContents();
@@ -51,8 +47,7 @@ function postFunction356($user356, $pass356, $method)
             preg_match_all('/([\w\d\.\-\_]+)@([\w\d\.\_\-]+)/mi', $string, $matches);
             $body->data = $matches[0];
         }
-    
-        //exibe dados do cliente
+
         if ($method == 'profile') {
             $response = $api->post(LOGIN, [
                 'form_params' => [
@@ -63,26 +58,27 @@ function postFunction356($user356, $pass356, $method)
             ]);
             return $response->getBody()->getContents();;
         }
-        
-        //exibe balanço
+
         if ($method == 'balance') {
             $response = $api->get(URI . BALANCE);
             $body->data = $response->getBody()->getContents();
         }
-        
-        //exibe dados do Servidor virtual de esportes
+
         if ($method == 'vsports') {
             $response = $api->get(URI . VSPORTS);
             return $response->getBody()->getContents();
         }
-        
-        //envia uma aposta
+
         if ($method == 'sendBet') {
             try {
                 $response = $api->post('https://www.bet365.com/' . PULLBET, [
                     'form_params' => [
                         'bs' => '1',
-                        'ns' => 'f=88965271#fp=751063548#st=40.00#',
+                        //'ns' => 'f=88960914#fp=750885681#st='.$betValue,
+                        'ns' => 'o='.$ls.'#ln='.$hc.'#f=88960920#fp=752036435#st=' . $betValue,
+                        //'ns' => '',
+                        'betsource' => 'FlashInPLay',
+                        'tagType' => 'WindowsDesktopBrowser'
                     ]
                 ]);
                 return $response->getBody()->getContents();
@@ -90,8 +86,7 @@ function postFunction356($user356, $pass356, $method)
                 return $body->data = $err;
             }
         }
-        
-        //adiciona aposta na fila (caderneta)
+
         if ($method == 'addBet') {
             try {
                 $response = $api->post('https://www.bet365.com/' . ADDBET, [
@@ -111,5 +106,4 @@ function postFunction356($user356, $pass356, $method)
     return json_encode($body);
 }
 
-//exibe respostas (em Json)
-echo postFunction356($post['user356'], $post['pass356'], $post['method']);
+echo postFunction356($post['user356'], $post['pass356'], $post['method'], $post['betValue'], $post['handCap'], $post['ls']);
